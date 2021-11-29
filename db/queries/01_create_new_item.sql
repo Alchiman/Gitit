958ef2672aff4c660986e0b1b20c10118cbb30c4
Luis Andres Contreras <tremenditomen@gmail.com>
2021-11-29 15:18:19 -0700

    adds new item to the menu

--- db/queries/01_create_new_item.sql
+++ db/queries/01_create_new_item.sql
@@ -1,3 +1,4 @@
+-- creats a new item
 SELECT * 
-FROM users
-WHERE is_admin = TRUE
+FROM items;
+INSERT INTO items (name,price,description,img_url,tag) VALUES ('egg',1,'is an egg','urlhere', 'best egg' );
