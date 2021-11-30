5e44c4b374e835fbc1a9bc3fc7120ce87a1419e6
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-11-29 19:50:13 -0800

    added orderacceptOrRejectOrder, cancelOrder, fulfillOrder

--- server/database.js
+++ server/database.js
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 // PG database client/connection setup
 const dbParams = require("../lib/db.js");
@@ -8,7 +8,7 @@ db.connect();
 
 const addNewItem = function(name, price, description, img_url, tag) {
   return db
-    .query(``,)
+    .query(``)
     .then(result => {
       return result.rows[0];
     })
@@ -18,7 +18,7 @@ exports.addNewItem = addNewItem;
 
 const updateItem = function(name, price, description, img_url, tag) {
   return db
-    .query(``,)
+    .query(``)
     .then(result => {
       return result.rows[0];
     })
@@ -28,7 +28,7 @@ exports.updateItem = updateItem;
 
 const deleteItem = function(name) {
   return db
-    .query(``,)
+    .query(``)
     .then(result => {
       return result.rows[0];
     })
@@ -36,19 +36,61 @@ const deleteItem = function(name) {
 };
 exports.deleteItem = deleteItem;
 
-const changeStatus = function(status, order_no) {
+const acceptOrRejectOrder = function(status, order_no) {
+  queryParams = [];
+  let queryString = `UPDATE orders SET status = $1 WHERE status = $2 AND order_number = $3;`;
+  if (status === "rejected") {
+    queryParams.push("rejected", "pending");
+  }
+  if (status === "accepted") {
+    queryParams.push("accepted", "pending");
+  }
+  queryParams.push(order_no);
   return db
-    .query(``,)
-    .then(result => {
-      return result.rows[0];
+    .query(queryString, queryParams)
+    .then(() => {
+      return true;
     })
     .catch(err => console.log(err.message));
 };
-exports.changeStatus = changeStatus;
+exports.acceptOrRejectOrder = acceptOrRejectOrder;
+
+const cancelOrder = function(order_no) {
+  return db
+    .query(
+      `UPDATE orders
+    SET status = 'canceled'
+    WHERE status = $1
+    OR status = $2
+    AND order_number = $3;`,
+      ["accepted", "pending", order_no]
+    )
+    .then(() => {
+      return true;
+    })
+    .catch(err => console.log(err.message));
+};
+exports.cancelOrder = cancelOrder;
+
+const fulfillOrder = function(order_no) {
+  return db
+    .query(
+      `UPDATE orders
+      SET status = 'fulfilled',
+      date_finished = NOW()
+      WHERE status = 'accepted' AND  order_number = $1;`,
+      [order_no]
+    )
+    .then(() => {
+      return true;
+    })
+    .catch(err => console.log(err.message));
+};
+exports.fulfillOrder = fulfillOrder;
 
 const getActiveOrders = function() {
   return db
-    .query(``,)
+    .query(``)
     .then(result => {
       return result.rows[0];
     })
@@ -58,7 +100,7 @@ exports.getActiveOrders = getActiveOrders;
 
 const getOrderHistory = function() {
   return db
-    .query(``,)
+    .query(``)
     .then(result => {
       return result.rows[0];
     })
@@ -69,7 +111,7 @@ exports.getOrderHistory = getOrderHistory;
 
 const getPhoneNumber = function(order_no) {
   return db
-    .query(``,)
+    .query(``)
     .then(result => {
       return result.rows[0];
     })
@@ -79,7 +121,7 @@ exports.getPhoneNumber = getPhoneNumber;
 
 const createOrder = function(itemList) {
   return db
-    .query(``,)
+    .query(``)
     .then(result => {
       return result.rows[0];
     })
@@ -89,7 +131,7 @@ exports.createOrder = createOrder;
 
 const getOrder = function(order_no) {
   return db
-    .query(``,)
+    .query(``)
     .then(result => {
       return result.rows[0];
     })
@@ -99,7 +141,7 @@ exports.getOrder = getOrder;
 
 const getUserInfo = function() {
   return db
-    .query(``,)
+    .query(``)
     .then(result => {
       return result.rows[0];
     })
@@ -109,7 +151,7 @@ exports.getUserInfo = getUserInfo;
 
 const updateUserInfo = function(name, email, phone) {
   return db
-    .query(``,)
+    .query(``)
     .then(result => {
       return result.rows[0];
     })
