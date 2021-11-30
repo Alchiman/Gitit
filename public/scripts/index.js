2940decac399a244a4343bb9b593096529755e84
Ivan <ivanxintang@gmail.com>
2021-11-30 11:40:41 -0800

    Add index.js to fetch menuItems and populate frontend

---
+++ public/scripts/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+// Client facing scripts here
+$(document).ready(function() {
+  getAllMenuItems().then(function(json) {
+    menuItems.addMenuItems(json);
+    views_manager.render('menuList');
+
+    //setup menu item button listeners
+    let itemCount = 0;
+    $("nav p").text(itemCount);
+    $(".card__footer button").on("click", function() {
+      itemCount++;
+      const itemName = $(this).parent().parent().find("p")[0].innerText;
+      orderSummary.addToCart(itemName);
+      $("nav p").text(itemCount);
+    });
+  });
+});
