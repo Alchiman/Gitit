f55869333573be438b652baafdaeb1a0de7d6a6c
Ivan <ivanxintang@gmail.com>
2021-12-01 10:00:41 -0800

    Added body parser

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}));
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -29,17 +37,16 @@ app.use(
 
 app.use(express.static("public"));
 
+
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
 const apiRoutes = require("./routes/apiRoutes");
 const adminsRoutes = require("./routes/admins");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/users", usersRoutes(database));
-app.use("/api/widgets", widgetsRoutes(database));
 app.use("/api", apiRoutes(database));
 app.use("/admins", adminsRoutes(database));
 // Note: mount other resources here, using the same pattern above
