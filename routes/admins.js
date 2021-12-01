643940591187f6c1eec7a49af2f65740c769c202
Luis Andres Contreras <tremenditomen@gmail.com>
2021-11-30 20:28:12 -0700

    admin routs

--- routes/admins.js
+++ routes/admins.js
@@ -19,5 +19,43 @@ module.exports = db => {
         res.status(500).json({ error: err.message });
       });
   });
+  router.get("/menus", (req, res) => {
+    db.getAllItems()
+      .then(data => {
+        const items = data;
+        console.log("data:",data)
+        res.json({ items });
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  router.post("/menus/create", (req, res) => {
+    const {name, price, description, img_url, tag} = req.body
+    console.log(name, price, description, img_url, tag)
+    db.addNewItem(name, Number(price), description, img_url, tag)
+      .then(data => {
+        const items = data[0];
+        console.log("data:",data[0])
+        res.json({ items });
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  router.post("/menus/edit", (req, res) => {
+    const {name, price, description, img_url, tag, originalName} = req.body
+    console.log(name, price, description, img_url, tag, originalName)
+    console.log('REQ.BODY:',req.body)
+    db.updateItem(name, price, description, img_url, tag, originalName)
+      .then(data => {
+        const items = data;
+        console.log("ITEMS:",items)
+        res.json({ items });
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
   return router;
 };
