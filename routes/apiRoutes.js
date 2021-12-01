e18e43e5e27b319919358755393700a8541c2e1c
Ivan <ivanxintang@gmail.com>
2021-12-01 11:35:51 -0800

    Add quick login/logout routes for demo purposes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
+  //DON'T DELETE YET
   router.get("/", (req, res) => {
+    console.log(req.session.userId);
     db.getAllItems()
       .then(data => {
         return res.send(data);
@@ -12,6 +14,20 @@ module.exports = (db) => {
           .status(500)
           .json({ error: err.message });
       });
+  });
+
+  router.post("/login", (req, res) => {
+    const { id } = req.body;
+    req.session.userId = id;
+    console.log(req.session.userId);
+    return res.send(req.session.userId);
+  });
+
+  router.post("/logout", (req, res) => {
+    console.log(req.session.userId);
+    res.clearCookie("userId");
+    req.session = null;
+    return res.send(true);
   });
   return router;
 };
