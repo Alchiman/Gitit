d8c6f3199e13b8903304f2dd0c10b192ac2c304d
Ivan <ivanxintang@gmail.com>
2021-12-01 16:14:31 -0800

    Moved some listeners to the model file

--- public/scripts/helpers/loadListeners.js
+++ public/scripts/helpers/loadListeners.js
@@ -22,25 +22,4 @@ $(() => {
   //   itemCount++;
   //   $("nav p").text(itemCount);
   // });
-
-  //set home as the active menu
-  const $home = $(".navbar__item:nth-child(3)");
-  $home.addClass('js-active');
-  let active = $home;
-
-  //Indicate which navigation menu is active
-  $(".navbar__item").on("click", function() {
-    if ($(this).text() === 'Profile') {
-      views_manager.render('profile');
-    } else if ($(this).text() === 'Home') {
-      views_manager.render('menuList');
-    } else {
-      $(".container--card").detach();
-      views_manager.render('orderSummary');
-    }
-
-    active.removeClass('js-active');
-    active = $(this);
-    active.addClass("js-active");
-  });
 });
