8e670e5cb46418c1694e6f9c1cbbc075c241943c
Luis Andres Contreras <tremenditomen@gmail.com>
2021-12-01 17:18:32 -0700

    added more admin routes

--- routes/admins.js
+++ routes/admins.js
@@ -8,117 +8,148 @@
 const express = require("express");
 const router = express.Router();
 
-module.exports = db => {
+module.exports = (db) => {
+  // this is the orders page
   router.get("/", (req, res) => {
-    db.createOrder(1, 6)
-      .then(data => {
-        res.json({ data });
-      })
-      .catch(err => {
-        res.status(500).json({ error: err.message });
-      });
+    console.log("ADMIN:", req.session.isAdmin);
+    if (req.session.isAdmin) {
+      db.getPendingAcceptedOrders()
+        .then((data) => {
+          const items = data;
+          console.log("data:", data);
+          return res.json({ items });
+        })
+        .catch((err) => {
+          return res.status(500).json({ error: err.message });
+        });
+    } else {
+      return res.status(403).send("not an admin");
+    }
   });
-
   router.get("/history", (req, res) => {
-    db.adminOrderHistory()
-      .then(data => {
-        return res.send({ data });
-      })
-      .catch(err => {
-        res.status(500).json({ error: err.message });
-      });
+    console.log("ADMIN:", req.session.isAdmin);
+    if (req.session.isAdmin) {
+      db.adminOrderHistory()
+        .then((data) => {
+          const items = data;
+          console.log("data:", data);
+          return res.json({ items });
+        })
+        .catch((err) => {
+          return res.status(500).json({ error: err.message });
+        });
+    } else {
+      return res.status(403).send("not an admin");
+    }
   });
 
   router.get("/menus", (req, res) => {
-    db.getAllItems()
-      .then(data => {
-        const items = data;
-        console.log("data:", data);
-        res.json({ items });
-      })
-      .catch(err => {
-        res.status(500).json({ error: err.message });
-      });
-  });
-
-  router.post("/menus/delete", (req, res) => {
-    const { name } = req.body;
-    db.deleteItem(name)
-      .then(data => {
-        return res.send({ data });
-      })
-      .catch(err => {
-        res.status(500).json({ error: err.message });
-      });
+    if (req.session.isAdmin) {
+      db.getAllItems()
+        .then((data) => {
+          const items = data;
+          console.log("data:", data);
+          res.json({ items });
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
+    } else {
+      return res.status(403).send("not an admin");
+    }
   });
 
   router.post("/menus/create", (req, res) => {
-    const { name, price, description, img_url, tag } = req.body;
-    console.log(name, price, description, img_url, tag);
-    db.addNewItem(name, Number(price), description, img_url, tag)
-      .then(data => {
-        const items = data[0];
-        console.log("data:", data[0]);
-        res.json({ items });
-      })
-      .catch(err => {
-        res.status(500).json({ error: err.message });
-      });
+    if (req.session.isAdmin) {
+      const { name, price, description, img_url, tag } = req.body;
+      console.log(name, price, description, img_url, tag);
+      db.addNewItem(name, Number(price), description, img_url, tag)
+        .then((data) => {
+          const items = data;
+          
+          return res.json({ items });
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
+    } else {
+      return res.status(403).send("not an admin");
+    }
   });
-
   router.post("/menus/edit", (req, res) => {
-    const { name, price, description, img_url, tag, originalName } = req.body;
-    db.updateItem(name, price, description, img_url, tag, originalName)
-      .then(data => {
-        return res.send({ data });
-      })
-      .catch(err => {
-        res.status(500).json({ error: err.message });
-      });
+    if (req.session.isAdmin) {
+      const { name, price, description, img_url, tag, originalName } = req.body;
+      console.log(name, price, description, img_url, tag, originalName);
+      console.log("REQ.BODY:", req.body);
+      db.updateItem(name, price, description, img_url, tag, originalName)
+        .then((data) => {
+          const items = data;
+          console.log("ITEMS:", items);
+          res.json({ items });
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
+    } else {
+      return res.status(403).send("not an admin");
+    }
   });
-
-  router.get("/orders", (req, res) => {
-    db.getPendingOrders()
-      .then(data => {
-        return res.send({ data });
-      })
-      .catch(err => {
-        res.status(500).json({ error: err.message });
-      });
+  router.post("/menus/delete", (req, res) => {
+    if (req.session.isAdmin) {
+      const { name } = req.body;
+      db.deleteItem(name)
+        .then((data) => {
+          return res.send({ data });
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
+    } else{
+      return res.status(403).send("not an admin");
+    }
   });
-
   router.post("/orders/accept", (req, res) => {
+    if (req.session.isAdmin) {
     const { status, order_no } = req.body;
     db.acceptOrRejectOrder(status, order_no)
-      .then(data => {
+      .then((data) => {
         return res.send({ data });
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(500).json({ error: err.message });
       });
+    } else{
+      return res.status(403).send("not an admin");
+    }
   });
-
   router.post("/orders/reject", (req, res) => {
+    if (req.session.isAdmin) {
     const { status, order_no } = req.body;
     db.acceptOrRejectOrder(status, order_no)
-      .then(data => {
+      .then((data) => {
         return res.send({ data });
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(500).json({ error: err.message });
       });
-  });
+    } else{
 
+      return res.status(403).send("not an admin");
+    }
+  });
   router.post("/orders/fulfill", (req, res) => {
+    if (req.session.isAdmin) {
     const { order_no } = req.body;
     db.fulfillOrder(order_no)
-      .then(data => {
+      .then((data) => {
         return res.send({ data });
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(500).json({ error: err.message });
       });
+    } else {
+      return res.status(403).send("not an admin");
+    }
   });
-
   return router;
 };
