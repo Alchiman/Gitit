4d181279fa151431b03991873395c0c8a8ee4a79
Luis Andres Contreras <tremenditomen@gmail.com>
2021-12-01 17:19:25 -0700

    checks to see if someone is admin

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
@@ -6,13 +6,11 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     console.log(req.session.userId);
     db.getAllItems()
-      .then(data => {
+      .then((data) => {
         return res.send(data);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
@@ -20,12 +18,33 @@ module.exports = (db) => {
     const { id } = req.body;
     req.session.userId = id;
     console.log(req.session.userId);
-    return res.send(req.session.userId);
+    db.isAdmin(id).then((data) => {
+      const isAdmin = data.is_admin;
+      console.log("ISADMIN:", isAdmin);
+      if (isAdmin === false) {
+        console.log("NOT AN ADMIN");
+
+         req.session.isAdmin = false;
+         console.log("AFTER SET FALSE:",req.session.isAdmin )
+         return res.send(req.session.isAdmin)
+        } else {
+          req.session.isAdmin = true;
+          console.log("AFTER SET TRUE:",req.session.isAdmin  )
+          
+          return res.send(req.session.isAdmin)
+          
+        }
+      });
+      console.log("before return",req.session.isAdmin  )
+//this was breaking it ↓ ↓ ↓ ↓ ↓ ↓ ↓ 
+    // return res.send(req.session.userId);
   });
 
   router.post("/logout", (req, res) => {
     console.log(req.session.userId);
+    console.log("ISADMIN:", req.session.isAdmin);
     res.clearCookie("userId");
+    res.clearCookie("isAdmin");
     req.session = null;
     return res.send(true);
   });
