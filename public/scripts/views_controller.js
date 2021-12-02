d5b075c7d1a12157b3f86e5ebef4e2090676017e
Ivan <ivanxintang@gmail.com>
2021-12-01 16:14:04 -0800

    Rename views_manager to views_controller for the MVC model

---
+++ public/scripts/views_controller.js
@@ -0,0 +1,70 @@
+/* eslint-disable indent */
+/* eslint-disable no-undef */
+$(() => {
+
+  const $main = $('#main-content');
+  const $nav = $('nav');
+  window.views_manager = {};
+
+  window.views_manager.navBarRender = function(component) {
+    $navbar_user.detach();
+    $navbar_admin.detach();
+
+    switch (component) {
+      case 'navBarUser':
+        $navbar_user.appendTo($nav);
+        break;
+      case 'navBarAdmin':
+        $navbar_admin.appendTo($nav);
+        break;
+      case 'error': {
+        const $error = $(`<p>${arguments[1]}</p>`);
+        $error.appendTo('body');
+        setTimeout(() => {
+          $error.remove();
+          views_manager.render('navBarUser');
+        }, 2000);
+        break;
+      }
+    }
+  };
+
+  window.views_manager.render = function(component) {
+    $menuList.detach();
+    $orderSummary.detach();
+    $profile.detach();
+    $orderRedirect.detach();
+    $orderReceipt.detach();
+
+    switch (component) {
+      case 'menuList':
+        $menuList.appendTo($main);
+        break;
+      case 'orderSummary':
+        $orderSummary.appendTo($main);
+        break;
+      case 'profile':
+        $profile.appendTo($main);
+        break;
+      case 'orderRedirect':
+        $orderRedirect.appendTo($main);
+        break;
+      case 'orderReceipt':
+        $orderReceipt.appendTo($main);
+        break;
+      case 'error': {
+        const $error = $(`<p>${arguments[1]}</p>`);
+        $error.appendTo('body');
+        setTimeout(() => {
+          $error.remove();
+          views_manager.render('menuList');
+        }, 2000);
+        break;
+      }
+    }
+  };
+});
+
+
+
+
